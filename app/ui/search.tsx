d4c0d94d1c06ad637625f0776782de189f986c59
Mica Linscheid <micascheid@gmail.com>
2023-12-18 15:49:54 -0700

    string vs String error

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,7 +8,7 @@ export default function Search({placeholder}: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
-    const handleSearch = useDebouncedCallback((term: String) => {
+    const handleSearch = useDebouncedCallback((term: string) => {
         const params = new URLSearchParams(searchParams);
         params.set('page', '1');
         if (term) {
